af7c2679fed1d93f37a4d1430615952a0599ccad
Hakan Tekir <hakan.tekir@adesso.com.tr>
2023-08-17 13:37:49 +0300

    feat: Split search view body into functions

--- MovieApp/Scenes/Search/SearchView.swift
+++ MovieApp/Scenes/Search/SearchView.swift
@@ -14,43 +14,19 @@ struct SearchView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                VStack {
-                    Asset.Colors.vibrantBlue.swiftUIColor
-                        .frame(height: 150)
-                    Spacer()
-                }
+                navigationBackground()
                 VStack {
                     HStack {
                         RoundedSearchField(label: L10n.searchSearchBar, text: $viewModel.query)
                     }.padding(.horizontal, 24)
                     if !viewModel.query.isEmpty {
-                        if let searchResults = viewModel.searchResults, !searchResults.isEmpty {
-                            ScrollView {
-                                ForEach(searchResults, id: \.id) { result in
-                                    SearchItemView(media: result)
-                                        .padding(.bottom, 20)
-                                }
-                            }
-                            .padding(.horizontal, 24)
-                            .padding(.top, 24)
+                        if let results = viewModel.searchResults,
+                           !results.isEmpty {
+                            searchResults(with: results)
                         } else if viewModel.isLoading {
-                            VStack {
-                                Spacer()
-                                ProgressView()
-                                Spacer()
-                            }
+                            loadingView()
                         } else {
-                            VStack {
-                                Spacer()
-                                Asset.Images.illustrationSearch.swiftUIImage
-                                Text(L10n.searchNone)
-                                    .font(.system(size: 18).bold())
-                                    .multilineTextAlignment(.center)
-                                    .lineSpacing(6)
-                                    .foregroundColor(Asset.Colors.vibrantBlue.swiftUIColor)
-                                    .frame(maxWidth: 200)
-                                Spacer()
-                            }
+                            anyResult()
                         }
                     }
                     Spacer()
@@ -66,6 +42,49 @@ struct SearchView: View {
             Task {
                 await viewModel.fetchResults()
             }
+        }
+    }
+}
+
+extension SearchView {
+    private func navigationBackground() -> some View {
+        VStack {
+            Asset.Colors.vibrantBlue.swiftUIColor
+                .frame(height: 150)
+            Spacer()
+        }
+    }
+
+    private func searchResults(with results: [SearchItemDataModel]) -> some View {
+        ScrollView {
+            ForEach(results, id: \.id) { result in
+                SearchItemView(media: result)
+                    .padding(.bottom, 20)
+            }
+        }
+        .padding(.horizontal, 24)
+        .padding(.top, 24)
+    }
+
+    private func loadingView() -> some View {
+        VStack {
+            Spacer()
+            ProgressView()
+            Spacer()
+        }
+    }
+
+    private func anyResult() -> some View {
+        VStack {
+            Spacer()
+            Asset.Images.illustrationSearch.swiftUIImage
+            Text(L10n.searchNone)
+                .font(.system(size: 18).bold())
+                .multilineTextAlignment(.center)
+                .lineSpacing(6)
+                .foregroundColor(Asset.Colors.vibrantBlue.swiftUIColor)
+                .frame(maxWidth: 200)
+            Spacer()
         }
     }
 }
